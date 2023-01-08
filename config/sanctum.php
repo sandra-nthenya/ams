148f1065edd9fb83f03be2cbe1360c73a1a1ee7b
Ava Njeri Goro <108757806+ViloCado@users.noreply.github.com>
2023-01-08 13:21:39 +0300

    Update sanctum.php

--- config/sanctum.php
+++ config/sanctum.php
@@ -1,14 +1,4 @@
 
-    /*
-    |--------------------------------------------------------------------------
-    | Stateful Domains
-    |--------------------------------------------------------------------------
-    |
-    | Requests from the following domains / hosts will receive stateful API
-    | authentication cookies. Typically, these should include your local
-    | and production domains which access your API via a frontend SPA.
-    |
-    */
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
