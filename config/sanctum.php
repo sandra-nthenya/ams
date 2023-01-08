7560fabafd5d6529a0f18182619e7eb11a2b54d2
Ava Njeri Goro <108757806+ViloCado@users.noreply.github.com>
2023-01-08 13:22:52 +0300

    Update sanctum.php

--- config/sanctum.php
+++ config/sanctum.php
@@ -8,9 +8,6 @@
 
    
 
-    'middleware' => [
-        'verify_csrf_token' => App\Http\Middleware\VerifyCsrfToken::class,
-        'encrypt_cookies' => App\Http\Middleware\EncryptCookies::class,
-    ],
+   
 
 ];
